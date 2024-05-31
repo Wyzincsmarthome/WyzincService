c38b10f5aaf9b0ec0be3de410e2bc25619b6cb98
Wyzincsmarthome <geral@wyzinc.pt>
2024-05-31 14:43:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,18 @@ const app = express();
 app.get('/', (req, res) => { res.send('Hello World!') });
 app.listen(3000, () => console.log('Server Running on Port 3000!'));
 
-import delay from 'delay';
+
 const fs = require('fs');
 const { getProductFromSupplier } = require('./functions/supplierAPI');
 const { createAuth, getAllProductsFromShopify, updateProductFromShopify, createProductToShopify } = require('./functions/shopifyAPI');
 
+function sleep(time, callback) {
+    var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        ;
+    }
+    callback();
+}
 
 async function executeAsyncTask () {
     /* > Criar Cliente Shopify */
@@ -42,7 +49,7 @@ async function executeAsyncTask () {
 
         /* > Criar ou Atualizar Produto na Shopify */      
         (tempShopifyProduct) ? await updateProductFromShopify(shopifyClient, tempShopifyProduct, productFromSupplier) : await createProductToShopify(shopifyClient, productFromSupplier);
-        await delay(1000);
+        sleep(1000, function() { console.log("") });
     }
     
     /* > 'Esvaziar' Variáveis */
