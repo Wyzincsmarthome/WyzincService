e22ff8eea400c77bd1b6433fe0272a621db35046
Wyzincsmarthome <geral@wyzinc.pt>
2024-06-04 13:01:05 +0100

    Corrigido para obter todos os produtos da loja

--- src/functions/shopifyAPI/getAllProductsFromShopify.js
+++ src/functions/shopifyAPI/getAllProductsFromShopify.js
@@ -1,10 +1,23 @@
 async function getAllProductsFromShopify(shopifyClient) {
-    const response = await shopifyClient.get("/products");
+    let response, responseResult, numProducts;
+    let finalResult = []
 
-    let finalResult;
-    (response) ? finalResult = await response.json() : finalResult = null;
+    response = await shopifyClient.get("/products/count");
+    numProducts = await response.json();
 
-    return finalResult.products;
+    let totalPages = Math.ceil(numProducts.count / 50);
+
+    for(let i = 1; i <= totalPages; i++) {
+        (i === 1)
+            ? response = await shopifyClient.get("/products")
+            : response = await shopifyClient.get("/products", { searchParams: { since_id: parseInt(finalResult[finalResult.length - 1].id) } });
+
+        (response) ? responseResult = await response.json() : responseResult = null;
+
+        finalResult.push(...responseResult.products);
+    }
+
+    return finalResult;
 }
 
 module.exports = getAllProductsFromShopify;
