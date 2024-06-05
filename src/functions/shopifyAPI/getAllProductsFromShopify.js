c0095491ace6577f9b37139eba2346d60d2030f4
Wyzincsmarthome <geral@wyzinc.pt>
2024-06-05 18:55:18 +0100

    Update getAllProductsFromShopify.js

--- src/functions/shopifyAPI/getAllProductsFromShopify.js
+++ src/functions/shopifyAPI/getAllProductsFromShopify.js
@@ -5,12 +5,12 @@ async function getAllProductsFromShopify(shopifyClient) {
     response = await shopifyClient.get("/products/count");
     numProducts = await response.json();
 
-    let totalPages = Math.ceil(numProducts.count / 50);
+    let totalPages = Math.ceil(numProducts.count / 250);
 
     for(let i = 1; i <= totalPages; i++) {
         (i === 1)
             ? response = await shopifyClient.get("/products")
-            : response = await shopifyClient.get("/products", { searchParams: { since_id: parseInt(finalResult[finalResult.length - 1].id) } });
+            : response = await shopifyClient.get("/products", { searchParams: { limit: 250, since_id: parseInt(finalResult[finalResult.length - 1].id) } });
 
         (response) ? responseResult = await response.json() : responseResult = null;
 
