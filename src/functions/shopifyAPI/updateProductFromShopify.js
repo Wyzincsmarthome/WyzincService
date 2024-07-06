b90c5b290cd190be1d574cd96762c4d2a2e0f1e1
RobertoValente <robertovalennte@gmail.com>
2024-07-06 12:26:22 +0100

    Atualizar preço de todas as variantes

--- src/functions/shopifyAPI/updateProductFromShopify.js
+++ src/functions/shopifyAPI/updateProductFromShopify.js
@@ -1,15 +1,38 @@
 require('colors');
 
 async function updateProductFromWix(shopifyClient, shopifyProduct, product) {
+
+    let productVariants = shopifyProduct.variants.map((variant) => {
+        return {
+            id: variant.id,
+            title: variant.title,
+            price: product.optFinalPrice,
+            sku: variant.sku,
+            position: variant.position,
+            // Sem certeza se estes campos são necessários:
+            /*inventory_policy: variant.inventory_policy,
+            compare_at_price: variant.compare_at_price,
+            fulfillment_service: variant.fulfillment_service,
+            inventory_management: variant.inventory_management,
+            option1: variant.option1,
+            option2: variant.option2,
+            option3: variant.option3,
+            taxable: variant.taxable,
+            barcode: variant.barcode,
+            grams: variant.grams,
+            weight: variant.weight,
+            weight_unit: variant.weight_unit,
+            inventory_quantity: variant.inventory_quantity,
+            old_inventory_quantity: variant.old_inventory_quantity,
+            requires_shipping: variant.requires_shipping,
+            image_id: variant.image_id,*/
+        }
+    });
+
     const response = await shopifyClient.put(`/products/${shopifyProduct.id}`, {
         data: {
             product: {
-                variants: [
-                    {
-                        id: shopifyProduct.variants[0].id,
-                        price: product.optFinalPrice
-                    }
-                ]
+                variants: productVariants
             }
         }
     });
